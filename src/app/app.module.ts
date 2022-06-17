63647963236d57995100e555d09208c2f5c7e0e7
Asjeus <anderson.santos.jesus@gmail.com>
2022-06-17 14:26:37 -0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from "@angular/common/http"
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
@@ -25,7 +26,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,32 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router:Router) { }
 
   ngOnInit(): void {
   }
 userModel = new User();
 
+mensagem=""
+
 
  receberDados(){
   console.log(this.userModel)
+
+  this.loginService.login(this.userModel).subscribe((Response) =>{
+    console.log("Deu certo")
+    localStorage.setItem("nomeUsuario", Response.body.user.nome)
+
+    //this.router.navigateByUrl("/")
+
+  }, (respostaErro) =>{
+    console.log ("Deu erro")
+
+   this.mensagem = respostaErro.error
+    //alert(respostaErro.error)
+    //alert("Erro")
+
+    
+  } )
  }
 }
